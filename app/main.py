cfbc09990cf4dc2797adfecba167f3c9013da8a1
muromtsev <t4k.org@mail.ru>
2023-08-19 09:33:13 +0300

    4.1 Basic auth

--- app/main.py
+++ app/main.py
@@ -1,36 +1,36 @@
-from fastapi import FastAPI, Cookie, Response
-# from .models.models import User # по идее импортируем так, зависит от структуры
-from pydantic import BaseModel # но для наглядности приведу единым текстом
-
+from fastapi import FastAPI, Depends, status, HTTPException
+from pydantic import BaseModel
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
+security = HTTPBasic()
 
+# модель пользователя
 class User(BaseModel):
     username: str
     password: str
 
-# имитируем хранилище юзеров
-sample_user: dict = {"username": "user123", "password": "password123"} # создали тестового юзера, якобы он уже зарегистрирован у нас
-fake_db: list[User] = [User(**sample_user)] # имитируем базу данных
-
-# имитируем хранилище сессий
-sessions: dict = {} # это можно хранить в кэше, например в Redis
+# добавим симуляцию базы данных в виде массива объектов юзеров
+USER_DATA = [User(**{"username": "user1", "password": "pass1"}), User(**{"username": "user2", "password": "pass2"})]
 
 
-#основная логика программы
-@app.post('/login')
-async def login(user: User, response: Response):
-    for person in fake_db: # перебрали юзеров в нашем примере базы данных
-        if person.username == user.username and person.password == user.password: # сверили логин и пароль
-            session_token = "abc123xyz456" # тут можно использовать модуль uuid (в продакшене), или модуль random (для выполнения задания), или самому написать рандомное значение куки, т.к. это пример тестовый
-            sessions[session_token] = user # сохранили у себя в словаре сессию, где токен - это ключ, а значение - объект юзера
-            response.set_cookie(key="session_token", value=session_token, httponly=True) # тут установили куки с защищенным флагом httponly - недоступны для вредоносного JS; флаг secure означает, что куки идут только по HTTPS
-            return {"message": "куки установлены"}
-    return {"message": "Invalid username or password"} # тут можно вернуть что хотите, в ТЗ не конкретезировалось, что делать, если логин/пароль неправильные
+# функция аутентификации
+def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
+    user = get_user_from_db(credentials.username)
+    if user is None or user.password != credentials.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    return user
 
-@app.get('/user')
-async def user_info(session_token = Cookie()):
-    user = sessions.get(session_token) # ищем в сессиях был ли такой токен создан, и если был, то возвращаем связанного с ним юзера
-    if user:
-        return user.dict() # у pydantic моделей есть метод dict(), который делает словарь из модели. Можно сразу хранить словарь в сессии, не суть
-    return {"message": "Unauthorized"}
+# логика получения информации о пользователе и его пароле
+# симуляционный пример
+def get_user_from_db(username: str):
+    for user in USER_DATA:
+        if user.username == username:
+            return user
+    return None
+
+# Защитита конечных точек с помощью аутентификации
+@app.get("/protected_resource/")
+def get_protected_resource(user: User = Depends(authenticate_user)):
+    return {"message": "You have access to the protected resource!", "user_info": user}
+
